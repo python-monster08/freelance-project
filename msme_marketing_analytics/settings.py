bc3381fc2cb730234b48abb007f6aa07319aa4c3
Kamlesh Lovewanshi <85796224+python-monster08@users.noreply.github.com>
2025-02-10 16:38:24 +0530

    Update settings.py
    Comment allow cors setting

--- msme_marketing_analytics/settings.py
+++ msme_marketing_analytics/settings.py
@@ -151,11 +151,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure CORS Settings
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React frontend
-    "http://127.0.0.1:3000",
-    "https://yourfrontend.com",
-]
+#CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:3000",  # React frontend
+#    "http://127.0.0.1:3000",
+#    "https://yourfrontend.com",
+#]
 
 
 # Configure DRF and JWT
