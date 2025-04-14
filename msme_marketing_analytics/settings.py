223fbb02b9da0b981ea8bc02f3ec73a1e4585d51
Kamlesh Lovewanshi <85796224+python-monster08@users.noreply.github.com>
2025-04-14 11:34:20 +0530

    Update settings.py

--- msme_marketing_analytics/settings.py
+++ msme_marketing_analytics/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'msme_marketing_analytics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
